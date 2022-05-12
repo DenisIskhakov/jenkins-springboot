a97b5eef5b9da48fc5673369749925e760851e13
DIskhakov <iskhakov.denis@mail.ru>
2022-05-12 16:00:47 +0300

    oo

--- src/main/java/com/example/jenkinsspringboot/controller/ThemeParkRideController.java
+++ src/main/java/com/example/jenkinsspringboot/controller/ThemeParkRideController.java
@@ -18,9 +18,7 @@ public class ThemeParkRideController {
         this.themeParkRideRepository = themeParkRideRepository;
     }
 
-    //
-//
-//
+
     @GetMapping(value = "/ride", produces = MediaType.APPLICATION_JSON_VALUE)
     public Iterable<ThemeParkRide> getRides() {
         return themeParkRideRepository.findAll();
